2cedc6ca3d9041b9a330cedae85b7d2fe133ad4a
Robert Carlsen <rwcarlsen@gmail.com>
2012-08-21 18:12:35 -0500

    started work on an agent that will export methods providing info about market supply/demand balances. It will be an agent just like any other and will (when I'm finished) register itself: sim.Engine.RegisterComm(self)

---
+++ agents/econ/econ.go
@@ -0,0 +1,65 @@
+
+type Request struct {
+  
+}
+
+func OfferBalance
+
+type Econ struct {
+   id string
+   req map[msg.Communicator]map[time.Time]float64
+   off map[msg.Communicator]map[time.Time]float64
+   Tracked []string
+   tm time.Time
+}
+
+func (e *Econ) SetId(id string) {
+  m.id = id
+}
+
+func (e *Econ) Id() string {
+  return m.id
+}
+
+func (e *Econ) Tick(eng *sim.Engine) {
+  e.tm = eng.Time()
+}
+
+func (e *Econ) Receive(m *msg.Message) {
+  if kind, ok := m.Payload.(string); ok {
+    commod := strings.Split("::").(string)
+    mkt := 
+  }
+}
+
+func (e *Econ) MsgNotify(m *msg.Message) {
+  if !e.isTracked(m.Owner) || m.Trans == nil {
+    return
+  }
+
+  qty = mg.Trans.Resource().Qty()
+  if mg.Trans.Type == trans.Offer {
+    e.off[m.Owner][e.tm] += qty
+  } else {
+    e.req[m.Owner][e.tm] += qty
+  }
+}
+
+func (e *Econ) isTracked(c msg.Communicator) bool {
+  if a, ok := c.(sim.Agent); ok {
+    for _, id := range e.Tracked {
+      if a.Id() == id {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+func (e *Econ) Parent() msg.Communicator {
+  return nil
+}
+
+func (e *Econ) SetParent(par msg.Communicator) {
+}
+
