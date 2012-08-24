eedb0f63c8e9d47148acd58ced5b5c48da8b315a
Robert Carlsen <rwcarlsen@gmail.com>
2012-08-24 13:25:43 -0500

    doc for rsrc pkg

--- rsrc/generic.go
+++ rsrc/generic.go
@@ -1,11 +1,15 @@
 
 package rsrc
 
-type Generic struct {
+type generic struct {
   units string
   qty float64
 }
 
+// NewGeneric returns a new generic resource initialized with qty of the given
+// units.
+//
+// Note that the specified units will be immutable.
 func NewGeneric(qty float64, units string) *Generic {
   return &Generic{
     units: units,
@@ -13,22 +17,27 @@ func NewGeneric(qty float64, units string) *Generic {
   }
 }
 
+// Type returns "Generic" for all generic resources.
 func (g *Generic) Type() string {
   return "Generic"
 }
 
+// Units returns the units specified at creation.
 func (g *Generic) Units() string {
   return g.units
 }
 
+// Qty returns the quantity the resource holds of [units].
 func (g *Generic) Qty() float64 {
   return g.qty
 }
 
+// SetQty changes the resources quantity to qty.
 func (g *Generic) SetQty(qty float64) {
   g.qty = qty
 }
 
+// Clone returns a deep-copy of the generic resource.
 func (g *Generic) Clone() Resource {
   clone := *g
   return &clone
