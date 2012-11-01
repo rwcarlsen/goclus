283bff85c47d5f6d61e39be18360e86432d46109
Robert Carlsen <rwcarlsen@gmail.com>
2012-11-01 12:25:33 -0500

    working on decay/branching algo and created convenience agent interface implementation for embedding (Id and SetId)

--- sim/sim.go
+++ sim/sim.go
@@ -13,6 +13,28 @@ type Agent interface {
 	Id() string
 }
 
+// Agenty is provided as a convenient way to automatically satisfy the Id and
+// SetId methods of the Agent interface.  Simply embed Agenty in the sim 
+// agent's struct:
+//
+//    type MyAgent struct {
+//       sim.Agenty
+//       ...
+//    }
+
+type Agenty string
+
+// Id returns the value passed via SetId or the empty string if it hasn't
+// been called.
+func (a Agenty) Id() string {
+  return string(a)
+}
+
+// SetId sets the value returned by Id.
+func (a Agenty) SetId(id string) {
+  a = Agenty(id)
+}
+
 type Starter interface {
 	Start(*Engine)
 }
